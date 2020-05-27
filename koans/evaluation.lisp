81ae15c4d45beaedfbc097828b4b045bc69433b3
Rico Trebeljahr <Nrin3141@users.noreply.github.com>
2020-05-27 11:08:35 +0200

    Solve evaluation lesson

--- koans/evaluation.lisp
+++ koans/evaluation.lisp
@@ -19,30 +19,30 @@
 
 (define-test function-names
   ;; In these examples, +, -, *, and / are function names.
-  (assert-equal ____ (+ 2 3))
-  (assert-equal ____ (- 1 3))
-  (assert-equal ____ (* 7 4))
-  (assert-equal ____ (/ 100 4)))
+  (assert-equal 5 (+ 2 3))
+  (assert-equal (- 2) (- 1 3))
+  (assert-equal 28 (* 7 4))
+  (assert-equal 25 (/ 100 4)))
 
 (define-test numberp
   ;; NUMBERP is a predicate which returns true if its argument is a number.
-  (assert-equal ____ (numberp 5))
-  (assert-equal ____ (numberp 2.0))
-  (assert-equal ____ (numberp "five")))
+  (assert-equal T (numberp 5))
+  (assert-equal T (numberp 2.0))
+  (assert-equal NIL (numberp "five")))
 
 (define-test evaluation-order
   ;; Arguments to a function are evaluated before the function is called.
-  (assert-equal ____ (* (+ 1 2) (- 13 10))))
+  (assert-equal 9 (* (+ 1 2) (- 13 10))))
 
 (define-test basic-comparisons
   ;; The below functions are boolean functions (predicates) that operate on
   ;; numbers.
-  (assert-equal ____ (> 25 4))
-  (assert-equal ____ (< 8 2))
-  (assert-equal ____ (= 3 3))
-  (assert-equal ____ (<= 6 (/ 12 2)))
-  (assert-equal ____ (>= 20 (+ 1 2 3 4 5)))
-  (assert-equal ____ (/= 15 (+ 4 10))))
+  (assert-equal T (> 25 4))
+  (assert-equal NIL (< 8 2))
+  (assert-equal T (= 3 3))
+  (assert-equal T (<= 6 (/ 12 2)))
+  (assert-equal T (>= 20 (+ 1 2 3 4 5)))
+  (assert-equal T (/= 15 (+ 4 10))))
 
 (define-test quote
   ;; Preceding a list with a quote (') will tell Lisp not to evaluate a list.
@@ -50,17 +50,17 @@
   ;; the literal list.
   ;; Evaluating the form (+ 1 2) returns the number 3, but evaluating the form
   ;; '(+ 1 2) returns the list (+ 1 2).
-  (assert-equal ____ (+ 1 2))
-  (assert-equal ____ '(+ 1 2))
-  (assert-equal ____ (list '+ 1 2))
+  (assert-equal 3 (+ 1 2))
+  (assert-equal '(+ 1 2) '(+ 1 2))
+  (assert-equal (list '+ 1 2) (list '+ 1 2))
   ;; The 'X syntax is syntactic sugar for (QUOTE X).
-  (true-or-false? ____ (equal '(/ 4 0) (quote (/ 4 0)))))
+  (true-or-false? T (equal '(/ 4 0) (quote (/ 4 0)))))
 
 (define-test listp
   ;; LISTP is a predicate which returns true if the argument is a list.
-  (assert-equal ____ (listp '(1 2 3)))
-  (assert-equal ____ (listp 100))
-  (assert-equal ____ (listp "Hello world"))
-  (assert-equal ____ (listp nil))
-  (assert-equal ____ (listp (+ 1 2)))
-  (assert-equal ____ (listp '(+ 1 2))))
+  (assert-equal T (listp '(1 2 3)))
+  (assert-equal NIL (listp 100))
+  (assert-equal NIL (listp "Hello world"))
+  (assert-equal T (listp nil))
+  (assert-equal NIL (listp (+ 1 2)))
+  (assert-equal T (listp '(+ 1 2))))
